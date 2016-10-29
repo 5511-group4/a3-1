c6d41e6bc2970c8d9e554c0a4312f7996e23b4b0
Lin <ray.linqh@gmail.com>
2016-10-28 22:51:19 -0400

    ts

---
+++ src/Node.java
@@ -0,0 +1,17 @@
+package As3;
+
+
+
+public class Node {
+	public Node( int data)
+	{
+		this.Data = data;
+	}
+	public int  Data = 0;
+	public Node  Left = null;
+	public Node  Right = null;
+	public Boolean  IsLeftThread=true;
+	public Boolean  IsRightThread=true;
+	
+}
+
